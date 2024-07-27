dd29c08949f661745eb18ff2079e3fbd09144a81
Dukepq <maikelvdj@live.be>
2024-07-27 22:40:11 +0200

    feat/style: improvements to footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -7,27 +7,53 @@ export default function Footer() {
   return (
     <footer className="bg-backdrop px-3 py-6">
       <div className="mx-auto max-w-screen-xl">
-        <div className="[&>*]:block text-text/80 [&>*]:transition-colors text-sm">
-          <Image className="w-8 mb-2" alt="logo" src={logo} />
+        <div className="[&_a]:underline [&_a]:text-text/80 [&_h3]:font-medium [&_h3]:text-lg flex flex-wrap gap-9">
+          <div>
+            <h3>Database</h3>
+            <ul>
+              <li>
+                <ProgressLink
+                  className="hover:text-text/100"
+                  href="/db/search/items"
+                >
+                  items
+                </ProgressLink>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h3>News</h3>
+            <ul>
+              <li>
+                <ProgressLink className="hover:text-text/100" href="news">
+                  patch notes
+                </ProgressLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <hr className="my-6 opacity-80" />
+
+        <div className="text-text/80 [&>*]:transition-colors text-sm flex sm:gap-6 gap-3 justify-center items-center flex-col sm:flex-row">
+          <div className="flex items-center gap-3">
+            <Image className="w-6" alt="logo" src={logo} />
+            <span>{`Copyright © 2024-${new Date(Date.now()).getFullYear()} ${
+              process.env.DOMAIN
+            }`}</span>
+          </div>
 
           <ProgressLink
-            className="hover:text-text/100 mb-1"
+            className="hover:text-text/100 underline"
             href={"/privacy-policy"}
           >
             Privacy policy
           </ProgressLink>
           <ProgressLink
-            className="hover:text-text/100 mb-1"
+            className="hover:text-text/100 underline"
             href={"/terms-and-conditions"}
           >
             Terms of service
           </ProgressLink>
-        </div>
-        <hr className="opacity-50 my-3" />
-        <div className="flex text-sm opacity-80">
-          <span className="mb-1 mx-auto">{`Copyright © 2024-${new Date(
-            Date.now()
-          ).getFullYear()} ${process.env.DOMAIN}`}</span>
         </div>
       </div>
     </footer>
